e66a826bef534e971186aa35bf2eeb2a4fc87a96
Angus McLauchlan <angusmclauchlan@IT-Solver-MacBook-Air.local>
2020-07-10 19:13:59 +1000

    new script

---
+++ g-suite-decrement-subscription.py
@@ -0,0 +1,11 @@
+# TO DO: create custom gam python modules: https://groups.google.com/d/topic/google-apps-manager/SF7WaRqKBIk
+
+bashCommand = "export OAUTHFILE=oauth2.txt-itsolver.net; /Users/angusmclauchlan/bin/gam/gam update resoldsubscription thealternativeboard.com.au Google-Apps-Unlimited seats 31 31"
+import subprocess
+process = subprocess.run(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=True, text=True)
+print (process.stdout, process.stderr)
+
+if process.stderr: 
+    print(process.stderr)
+else:
+    print(process.stdout)
--- g-suite-increment-subscription.py
+++ g-suite-increment-subscription.py
@@ -1,11 +1,12 @@
 # TO DO: create custom gam python modules: https://groups.google.com/d/topic/google-apps-manager/SF7WaRqKBIk
 
-bashCommand = "export OAUTHFILE=oauth2.txt-itsolver.net; /Users/angusmclauchlan/bin/gam/gam update resoldsubscription thealternativeboard.com.au Google-Apps-Unlimited seats 32 32"
 import subprocess
-process = subprocess.run(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=True, text=True)
-print (process.stdout, process.stderr)
+bashCommand = "export OAUTHFILE=oauth2.txt-itsolver.net; /Users/angusmclauchlan/bin/gam/gam update resoldsubscription thealternativeboard.com.au Google-Apps-Unlimited seats 33 33"
+process = subprocess.run(bashCommand, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, shell=True, check=True, text=True)
+print(process.stdout, process.stderr)
 
-if process.stderr: 
+if process.stderr:
     print(process.stderr)
 else:
     print(process.stdout)
