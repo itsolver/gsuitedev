63e69438ca50017788806b70c444588614812870
Angus McLauchlan <angusmclauchlan@IT-Solver-MacBook-Air.local>
2020-07-22 14:24:14 +1000

    use newQuantity variable in string

--- g-suite-increment-subscription.py
+++ g-suite-increment-subscription.py
@@ -1,7 +1,9 @@
 # TO DO: create custom gam python modules: https://groups.google.com/d/topic/google-apps-manager/SF7WaRqKBIk
 
 import subprocess
-bashCommand = "export OAUTHFILE=oauth2.txt-itsolver.net; /Users/angusmclauchlan/bin/gam/gam update resoldsubscription thealternativeboard.com.au Google-Apps-Unlimited seats 33 33"
+newQuantity = ""
+bashCommand = "export OAUTHFILE=oauth2.txt-itsolver.net; /Users/angusmclauchlan/bin/gam/gam update resoldsubscription thealternativeboard.com.au Google-Apps-Unlimited seats {} {}".format(
+    newQuantity, newQuantity)
 process = subprocess.run(bashCommand, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE, shell=True, check=True, text=True)
 print(process.stdout, process.stderr)
